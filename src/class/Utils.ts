ab70d5182bab787658fa68de006590c931225db9
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2024-11-06 23:26:40 -0300

    Add function to get initials from a composed or single name

--- src/class/Utils.ts
+++ src/class/Utils.ts
@@ -1,8 +1,17 @@
 export class Utils {
   getInitials(projectName: string): string {
-    return projectName
+    const words = projectName
+      .trim()
       .split(" ")
+      .filter((word) => word.length > 0);
+
+    if (words.length === 1) {
+      return words[0].slice(0, 2).toUpperCase();
+    }
+
+    return words
       .map((word) => word.charAt(0).toUpperCase())
+      .slice(0, 2)
       .join("");
   }
 }
