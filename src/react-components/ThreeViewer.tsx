55cb8cb3f5fd8e7942613e61602f3cf49fc1dee8
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2025-05-03 19:11:19 -0300

    Dispose objects

--- src/react-components/ThreeViewer.tsx
+++ src/react-components/ThreeViewer.tsx
@@ -8,15 +8,29 @@ import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js";
 import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader.js";
 
 export function ThreeViewer() {
-  useEffect(() => {
-    const scene = new THREE.Scene();
+  let scene: THREE.Scene | null;
+  let mesh: THREE.Object3D | null;
+  let renderer: THREE.WebGLRenderer | null;
+  let cameraControls: OrbitControls | null;
+  let camera: THREE.PerspectiveCamera | null;
+  let axes: THREE.AxesHelper | null;
+  let grid: THREE.GridHelper | null;
+  let directionalLight: THREE.DirectionalLight | null;
+  let ambientLight: THREE.AmbientLight | null;
+  let mtlLoader: MTLLoader | null;
+  let objLoader: OBJLoader | null;
+
+  const setViewer = () => {
+    scene = new THREE.Scene();
+
     const viewerContainer = document.getElementById(
       "viewer-container"
     ) as HTMLElement;
-    const camera = new THREE.PerspectiveCamera(75);
+
+    camera = new THREE.PerspectiveCamera(75);
     camera.position.z = 5;
 
-    const renderer = new THREE.WebGLRenderer({
+    renderer = new THREE.WebGLRenderer({
       alpha: true,
       antialias: true,
     });
@@ -25,9 +39,14 @@ export function ThreeViewer() {
 
     function resizeViewer() {
       const containerDimensions = viewerContainer.getBoundingClientRect();
+
+      if (!renderer) return;
       renderer.setSize(containerDimensions.width, containerDimensions.height);
+
       const aspectRatio =
         containerDimensions.width / containerDimensions.height;
+
+      if (!camera) return;
       camera.aspect = aspectRatio;
       camera.updateProjectionMatrix();
     }
@@ -36,27 +55,24 @@ export function ThreeViewer() {
 
     resizeViewer();
 
-    const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshStandardMaterial();
-    const cube = new THREE.Mesh(boxGeometry, material);
-
-    const directionalLight = new THREE.DirectionalLight();
-    const ambientLight = new THREE.AmbientLight();
+    directionalLight = new THREE.DirectionalLight();
+    ambientLight = new THREE.AmbientLight();
     ambientLight.intensity = 0.4;
 
     scene.add(directionalLight, ambientLight);
 
-    const cameraControls = new OrbitControls(camera, viewerContainer);
+    cameraControls = new OrbitControls(camera, viewerContainer);
 
     function renderScene() {
+      if (!renderer || !scene || !camera) return;
       renderer.render(scene, camera);
       requestAnimationFrame(renderScene);
     }
 
     renderScene();
 
-    const axes = new THREE.AxesHelper();
-    const grid = new THREE.GridHelper();
+    axes = new THREE.AxesHelper();
+    grid = new THREE.GridHelper();
 
     grid.material.transparent = true;
     grid.material.opacity = 0.4;
@@ -64,31 +80,36 @@ export function ThreeViewer() {
 
     scene.add(axes, grid);
 
-    const gui = new GUI();
-    const cubeControls = gui.addFolder("Cube");
-
-    cubeControls.add(cube.position, "x", -10, 10, 1);
-    cubeControls.add(cube.position, "y", -10, 10, 1);
-    cubeControls.add(cube.position, "z", -10, 10, 1);
-    cubeControls.add(cube, "visible");
-    cubeControls.addColor(cube.material, "color");
-    cubeControls.add(cube.material, "wireframe");
-    cubeControls.add(cube.rotation, "x", 0, Math.PI * 2, 0.1);
-    cubeControls.add(cube.rotation, "y", 0, Math.PI * 2, 0.1);
-    cubeControls.add(cube.rotation, "z", 0, Math.PI * 2, 0.1);
-    cubeControls.add(cube.material, "metalness", 0, 1, 0.01);
-
-    const objLoader = new OBJLoader();
-    const mtlLoader = new MTLLoader();
+    objLoader = new OBJLoader();
+    mtlLoader = new MTLLoader();
 
     mtlLoader.load("../assets/Gear/Gear1.mtl", (materials) => {
       materials.preload();
+
+      if (!objLoader) return;
       objLoader.setMaterials(materials);
 
-      objLoader.load("../assets/Gear/Gear1.obj", (mesh) => {
-        scene.add(mesh);
+      objLoader.load("../assets/Gear/Gear1.obj", (object) => {
+        if (!scene) return;
+        scene.add(object);
+        mesh = object;
       });
     });
+  };
+
+  useEffect(() => {
+    setViewer();
+
+    return () => {
+      mesh?.removeFromParent();
+      mesh?.traverse((child) => {
+        if (child instanceof THREE.Mesh) {
+          child.geometry.dispose();
+          child.material.dispose();
+        }
+      });
+      mesh = null;
+    };
   }, []);
 
   return (
