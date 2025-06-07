83eb237b209ce832d1ffdb2f8eac1183c1850de5
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2025-06-07 00:03:42 -0300

    Add world panel

--- src/react-components/IFCViewer.tsx
+++ src/react-components/IFCViewer.tsx
@@ -5,6 +5,7 @@ import * as OBCF from "@thatopen/components-front";
 import * as BUI from "@thatopen/ui";
 import * as CUI from "@thatopen/ui-obc";
 import { FragmentsGroup } from "@thatopen/fragments"
+import { float } from "three/examples/jsm/nodes/Nodes.js";
 
 export function IFCViewer() {
   const components = new OBC.Components();
@@ -32,6 +33,7 @@ export function IFCViewer() {
 
     components.init()
 
+    world.renderer.postproduction.enabled = true;
     world.camera.controls.setLookAt(3,3,3,0,0,0)
     world.camera.updateAspect();
 
@@ -123,16 +125,29 @@ export function IFCViewer() {
     });
 
     const elementPropertyPanel = BUI.Component.create<BUI.Panel>(() => {
+      const [propsTable, updatePropsTable] = CUI.tables.elementProperties({
+        components,
+        fragmentIdMap: {}
+      })
+
       const highlighter = components.get(OBCF.Highlighter)
       highlighter.events.select.onHighlight.add((fragmentIdMap) => {
         if (!floatingGrid) return
         floatingGrid.layout = "second"
+        updatePropsTable({ fragmentIdMap })
+        propsTable.expanded = false
       })
 
       highlighter.events.select.onClear.add(() => {
+        updatePropsTable({ fragmentIdMap: {} })
         if (!floatingGrid) return
         floatingGrid.layout = "main"
       })
+
+      const search = (e: Event) => {
+        const input = e.target as BUI.TextInput
+        propsTable.queryString = input.value;
+      }
 
       return BUI.html`
         <bim-panel>
@@ -141,15 +156,53 @@ export function IFCViewer() {
             label="Property Information"
             icon="solar:document-bold"
             fixed
-          ></bim-panel-section>
+          >
+            <bim-text-input @input=${search} placeholder="Search..."></bim-text-input>
+            ${propsTable}
+          </bim-panel-section>
         </bim-panel>
       `;
     })
+
+    const onWorldsUpdate = () => {
+      if (!floatingGrid) return;
+      floatingGrid.layout = "world";
+    }
+
+    const worldPanel = BUI.Component.create<BUI.Panel>(() => {
+      const [worldTable] = CUI.tables.worldsConfiguration({ components })
+
+      const search = (e: Event) => {
+        const input = e.target as BUI.TextInput
+        worldTable.queryString = input.value;
+      }
+    
+    return BUI.html`
+      <bim-panel>
+        <bim-panel-section
+          name="world"
+          label="World Information"
+          icon="solar:document-bold"
+          fixed
+        >
+          <bim-text-input @input=${search} placeholder="Search..."></bim-text-input>
+          ${worldTable}
+        </bim-panel-section>
+      </bim-panel>
+    `;
+  })
 
     const toolbar = BUI.Component.create<BUI.Toolbar>(() => {
     const [loadIfcBtn] = CUI.buttons.loadIfc({ components: components })
     return BUI.html`
       <bim-toolbar style="justify-self: center;">
+        <bim-toolbar-section label="App">
+          <bim-button
+            label="World"
+            icon="tabler:brush"
+            @click=${onWorldsUpdate}
+          ></bim-button>
+        </bim-toolbar-section>
         <bim-toolbar-section label="Import">
           ${loadIfcBtn}
         </bim-toolbar-section>
@@ -201,6 +254,17 @@ export function IFCViewer() {
         elements: {
           toolbar,
           elementPropertyPanel
+        }
+      },
+      world: {
+        template: `
+          "empty worldPanel" 1fr
+          "toolbar toolbar" auto
+          /1fr 20rem
+        `,
+        elements: {
+          toolbar,
+          worldPanel
         }
       }
     }
